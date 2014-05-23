f425d63833e827687a6235cd7d27d36bbd6cc6f9
Steve Wilburn <stevewilburn@Steves-MacBook-Air.local>
2014-05-22 17:28:51 -0700

    made url filters a bit prettier

--- background.js
+++ background.js
@@ -2,4 +2,5 @@ function interceptRequest(request) {
     return { redirectUrl: 'https://cdn.mxpnl.com/libs/mixpanel-2.2.js' }
 }
         
-chrome.webRequest.onBeforeRequest.addListener(interceptRequest, { urls: ['https://cdn.mxpnl.com/libs/mixpanel-2.2.min.js','http://cdn.mxpnl.com/libs/mixpanel-2.2.min.js'] }, ['blocking']);
+var target_url = "cdn.mxpnl.com/libs/mixpanel-2.2.min.js"
+chrome.webRequest.onBeforeRequest.addListener(interceptRequest, { urls: ['https://'+target_url,'http://'+target_url] }, ['blocking']);
